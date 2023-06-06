ea63bf805bcb1230904f56e4b9eaccd03b01b834
juslong <juslong@umich.edu>
2023-06-06 10:33:58 -0400

    Add production URL for API

--- src/utils/fetchTruckData.ts
+++ src/utils/fetchTruckData.ts
@@ -4,7 +4,8 @@ import axios from 'axios';
 // Relative Dependencies
 import { TruckData } from '../types';
 
-const FOOD_TRUCK_API_URL = 'http://localhost:3001/food-trucks';
+const FOOD_TRUCK_API_URL =
+  'https://elc-engineering-challenge-production.up.railway.app/food-trucks';
 
 const fetchTruckData = async (): Promise<Array<TruckData | null>> => {
   const response = await axios.get(FOOD_TRUCK_API_URL);
